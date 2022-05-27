1ca6e0c61a80e3e5aaad7e69c215d1d47c88bb9e
DEENNO <yuntteng31@gmail.com>
2022-05-28 03:35:04 +0900

    keyFrames

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,21 @@ const Box = styled.div`
 	height: 200px;
 	width: 200px;
 	background-color: tomato;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	animation: ${animation} 1s linear infinite;
+	span {
+		font-size: 40px;
+	}
 `;
 
 function App() {
 	return (
 		<Wrapper>
-			<Box></Box>
+			<Box>
+				<span>🥰</span>
+			</Box>
 		</Wrapper>
 	);
 }

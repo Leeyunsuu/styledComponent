24b5193dde820b2a68df041d55d7a9d4edd80cb0
DEENNO <yuntteng31@gmail.com>
2022-05-28 03:21:59 +0900

    styled general

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, {keyframes} from "styled-components";
+
+const Wrapper = styled.div`
+	display: flex;
+`;
+
+const animation = keyframes`
+0%{
+  transform:rotate(0deg);
+  border-radius:0px;
+}
+ 50% {
+  border-radius:100px;
+}
+100%{
+transform:rotate(360deg);
+  border-radius:0px;
+}`;
+
+const Box = styled.div`
+	height: 200px;
+	width: 200px;
+	background-color: tomato;
+	animation: ${animation} 1s linear infinite;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Wrapper>
+			<Box></Box>
+		</Wrapper>
+	);
 }
 
 export default App;
